f5bff425cbdafea4c86a0a19fb3779fc9e8c9a0d
4ryasena <foryoublueee@gmail.com>
2022-04-13 18:16:55 +0700

    update
    adding community.html and others changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+// Phase reveal
 const phase = document.querySelectorAll('.phase')
-
 phase.forEach(e => {
    e.addEventListener('click', () => {
       e.classList.toggle('down')
@@ -8,10 +8,18 @@ phase.forEach(e => {
    })
 })
 
+// Dropdown the navbar
 const navDown = document.querySelector('.navDrop')
 const dropdown = document.querySelector('.dropdown')
-
 navDown.addEventListener('click', () => {
    dropdown.classList.toggle('show')
    console.log('haheah')
+})
+
+// removing dropdown nav when click his child
+const link = dropdown.querySelectorAll('.link')
+link.forEach(e => {
+   e.addEventListener('click', () => {
+      dropdown.classList.remove('show')
+   })
 })
